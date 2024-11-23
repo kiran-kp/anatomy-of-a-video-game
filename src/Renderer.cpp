151c282b4a4b05cffce9989ce8e01ad162e6a803
Kiran Purushothaman <projects@kirankp.com>
2024-11-23 14:54:42 -0800

    Split triangle rendering into a separate class as an exercise

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -16,10 +16,31 @@
 // Using a #define here because this is used to set uint32_t or size_t in different contexts and I didn't want cast it every time.
 #define NUM_BACKBUFFERS 2
 
-struct Vertex
+class TriangleRenderer
 {
-    DirectX::XMFLOAT3 position;
-    DirectX::XMFLOAT4 color;
+public:
+    TriangleRenderer() = default;
+    ~TriangleRenderer();
+
+    void Initialize(ID3D12Device* device, float width, float height);
+
+    void Render(ID3D12GraphicsCommandList* commandList, uint32_t frameIndex);
+
+private:
+
+    struct Vertex
+    {
+        DirectX::XMFLOAT3 position;
+        DirectX::XMFLOAT4 color;
+    };
+
+    CD3DX12_VIEWPORT mViewport;
+    CD3DX12_RECT mScissorRect;
+
+    ID3D12RootSignature* mRootSignature;
+    ID3D12PipelineState* mPipelineState;
+    ID3D12Resource* mVertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW mVertexBufferView;
 };
 
 class RendererImpl
@@ -34,8 +55,7 @@ public:
     void CreateCommandList();
     void CreateFence();
 
-    // We only have one shader at this point so hard code it in the renderer
-    void LoadShader();
+    void InitializeTriangleRenderer();
 
     void PopulateCommandListAndSubmit();
     void Present();
@@ -47,11 +67,8 @@ private:
     IDXGISwapChain3* mSwapChain;
     ID3D12CommandAllocator* mCommandAllocator;
 
-    CD3DX12_VIEWPORT mViewport;
-    CD3DX12_RECT mScissorRect;
     uint32_t mWidth;
     uint32_t mHeight;
-    float mAspectRatio;
 
     ID3D12DescriptorHeap* mRtvHeap;
     uint32_t mRtvDescriptorSize;
@@ -59,11 +76,7 @@ private:
     ID3D12Resource* mRenderTargets[NUM_BACKBUFFERS];
     ID3D12GraphicsCommandList* mCommandList;
 
-    ID3D12RootSignature* mRootSignature;
-    ID3D12PipelineState* mPipelineState;
-    ID3D12Resource* mVertexBuffer;
-    D3D12_VERTEX_BUFFER_VIEW mVertexBufferView;
-
+    TriangleRenderer mTriangleRenderer;
 
     uint32_t mFrameIndex;
     ID3D12Fence* mFence;
@@ -82,8 +95,9 @@ void Renderer::Initialize(Window& window)
     mImpl->CreateCommandQueue();
     mImpl->CreateSwapChain(window.GetHandle(), window.GetWidth(), window.GetHeight());
     mImpl->CreateCommandList();
-    mImpl->LoadShader();
     mImpl->CreateFence();
+
+    mImpl->InitializeTriangleRenderer();
 
     // Wait for all the setup work we just did to complete because we are going to re-use the command list
     mImpl->WaitForPreviousFrame();
@@ -140,18 +154,6 @@ void RendererImpl::CreateSwapChain(HWND hwnd, uint32_t width, uint32_t height)
 
     mWidth = width;
     mHeight = height;
-    mAspectRatio = static_cast<float>(width) / static_cast<float>(height);
-    mViewport.TopLeftX = 0.0f;
-    mViewport.TopLeftY = 0.0f;
-    mViewport.Width = static_cast<float>(width);
-    mViewport.Height = static_cast<float>(height);
-    mViewport.MinDepth = D3D12_MIN_DEPTH;
-    mViewport.MaxDepth = D3D12_MAX_DEPTH;
-
-    mScissorRect.left = 0;
-    mScissorRect.top = 0;
-    mScissorRect.right= width;
-    mScissorRect.bottom = height;
 
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
     swapChainDesc.BufferCount = NUM_BACKBUFFERS;
@@ -203,8 +205,110 @@ void RendererImpl::CreateSwapChain(HWND hwnd, uint32_t width, uint32_t height)
     mDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&mCommandAllocator));
 }
 
-void RendererImpl::LoadShader()
+void RendererImpl::CreateCommandList()
 {
+    ensure(SUCCEEDED(mDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, mCommandAllocator, nullptr, IID_PPV_ARGS(&mCommandList))));
+
+    // The command list is in the recording state when it is created but we have nothing do to at the moment.
+    mCommandList->Close();
+}
+
+// A fence is a synchronization primitive that we can use to signal that the GPU is done rendering a frame
+void RendererImpl::CreateFence()
+{
+    ensure(SUCCEEDED(mDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&mFence))));
+    mFenceValue = 1;
+
+    // This is the event we will listen for
+    mFenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+    if (mFenceEvent == nullptr)
+    {
+        ensure(SUCCEEDED(HRESULT_FROM_WIN32(GetLastError())));
+    }
+}
+
+void RendererImpl::InitializeTriangleRenderer()
+{
+    mTriangleRenderer.Initialize(mDevice, static_cast<float>(mWidth), static_cast<float>(mHeight));
+}
+
+void RendererImpl::PopulateCommandListAndSubmit()
+{
+    // This should only be done after the command list associated with this allocator has finished execution.
+    // Ensure that this is only called after WaitForPreviousFrame().
+    ensure(SUCCEEDED(mCommandAllocator->Reset()));
+
+    // This sets it back to the recording state so we can set up our frame
+    ensure(SUCCEEDED(mCommandList->Reset(mCommandAllocator, nullptr)));
+
+    // Transition our back buffer to be able to be used as a render target since we're rendering to it
+    mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mRenderTargets[mFrameIndex], D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(mRtvHeap->GetCPUDescriptorHandleForHeapStart(), mFrameIndex, mRtvDescriptorSize);
+    mCommandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
+
+    const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+    mCommandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+    mTriangleRenderer.Render(mCommandList, mFrameIndex);
+
+    // Transition back buffer back to the present state since we are done drawing to it and want it ready for present
+    mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mRenderTargets[mFrameIndex], D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+    ensure(SUCCEEDED(mCommandList->Close()));
+
+    ID3D12CommandList* ppCommandLists[] = { mCommandList };
+    mCommandQueue->ExecuteCommandLists(1, ppCommandLists);
+}
+
+void RendererImpl::Present()
+{
+    ensure(SUCCEEDED(mSwapChain->Present(1, 0)));
+}
+
+void RendererImpl::WaitForPreviousFrame()
+{
+    // TODO: This is apparently not a good way to queue up work for the GPU because we're doing nothing while we wait for the previous frame to render.
+    // Look into the frame buffering sample
+
+    // Signal and increment the fence value.
+    const UINT64 fence = mFenceValue;
+    ensure(SUCCEEDED(mCommandQueue->Signal(mFence, fence)));
+    mFenceValue++;
+
+    // Wait until the previous frame is finished.
+    if (mFence->GetCompletedValue() < fence)
+    {
+        ensure(SUCCEEDED(mFence->SetEventOnCompletion(fence, mFenceEvent)));
+        WaitForSingleObject(mFenceEvent, INFINITE);
+    }
+
+    mFrameIndex = mSwapChain->GetCurrentBackBufferIndex();
+}
+
+TriangleRenderer::~TriangleRenderer()
+{
+    mRootSignature->Release();
+    mPipelineState->Release();
+    mVertexBuffer->Release();
+}
+
+void TriangleRenderer::Initialize(ID3D12Device* device, float width, float height)
+{
+    float aspectRatio = width / height;
+    mViewport.TopLeftX = 0.0f;
+    mViewport.TopLeftY = 0.0f;
+    mViewport.Width = width;
+    mViewport.Height = height;
+    mViewport.MinDepth = D3D12_MIN_DEPTH;
+    mViewport.MaxDepth = D3D12_MAX_DEPTH;
+
+    mScissorRect.left = 0;
+    mScissorRect.top = 0;
+    mScissorRect.right= static_cast<uint64_t>(width);
+    mScissorRect.bottom = static_cast<uint64_t>(height);
+
+
     // Create a root signature. This data structure describes what resources are bound to the pipeline at each shader stage.
     // In our case, we don't need anything yet.
     {
@@ -218,7 +322,7 @@ void RendererImpl::LoadShader()
         ID3DBlob* signature;
         ID3DBlob* error;
         ensure(SUCCEEDED(D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error)));
-        ensure(SUCCEEDED(mDevice->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&mRootSignature))));
+        ensure(SUCCEEDED(device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&mRootSignature))));
     }
 
     // Create our pipeline
@@ -258,16 +362,16 @@ void RendererImpl::LoadShader()
         psoDesc.NumRenderTargets = 1;
         psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
         psoDesc.SampleDesc.Count = 1;
-        ensure(SUCCEEDED(mDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mPipelineState))));
+        ensure(SUCCEEDED(device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mPipelineState))));
     }
 
     // Set up the vertex buffers here for now since this shader is very basic and not doing anything interesting
     {
         Vertex triangleVertices[] =
         {
-            { { 0.0f, 0.25f * mAspectRatio, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
-            { { 0.25f, -0.25f * mAspectRatio, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
-            { { -0.25f, -0.25f * mAspectRatio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+            { { 0.0f, 0.25f * aspectRatio, 0.0f }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+            { { 0.25f, -0.25f * aspectRatio, 0.0f }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+            { { -0.25f, -0.25f * aspectRatio, 0.0f }, { 0.0f, 0.0f, 1.0f, 1.0f } }
         };
 
         const uint32_t vertexBufferSize = sizeof(triangleVertices);
@@ -279,7 +383,7 @@ void RendererImpl::LoadShader()
         // recommended. Every time the GPU needs it, the upload heap will be marshalled 
         // over. Please read up on Default Heap usage. An upload heap is used here for 
         // code simplicity and because there are very few verts to actually transfer.
-        ensure(SUCCEEDED(mDevice->CreateCommittedResource(&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+        ensure(SUCCEEDED(device->CreateCommittedResource(&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
                                                           D3D12_HEAP_FLAG_NONE,
                                                           &CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
                                                           D3D12_RESOURCE_STATE_GENERIC_READ,
@@ -301,85 +405,15 @@ void RendererImpl::LoadShader()
     }
 }
 
-void RendererImpl::CreateCommandList()
+void TriangleRenderer::Render(ID3D12GraphicsCommandList* commandList, uint32_t frameIndex)
 {
-    ensure(SUCCEEDED(mDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, mCommandAllocator, mPipelineState, IID_PPV_ARGS(&mCommandList))));
-
-    // The command list is in the recording state when it is created but we have nothing do to at the moment.
-    mCommandList->Close();
-}
-
-// A fence is a synchronization primitive that we can use to signal that the GPU is done rendering a frame
-void RendererImpl::CreateFence()
-{
-    ensure(SUCCEEDED(mDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&mFence))));
-    mFenceValue = 1;
-
-    // This is the event we will listen for
-    mFenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
-    if (mFenceEvent == nullptr)
-    {
-        ensure(SUCCEEDED(HRESULT_FROM_WIN32(GetLastError())));
-    }
-}
-
-void RendererImpl::PopulateCommandListAndSubmit()
-{
-    // This should only be done after the command list associated with this allocator has finished execution.
-    // Ensure that this is only called after WaitForPreviousFrame().
-    ensure(SUCCEEDED(mCommandAllocator->Reset()));
-
-    // This sets it back to the recording state so we can set up our frame
-    ensure(SUCCEEDED(mCommandList->Reset(mCommandAllocator, mPipelineState)));
-
-    // Set up state
-    mCommandList->SetGraphicsRootSignature(mRootSignature);
-    mCommandList->RSSetViewports(1, &mViewport);
-    mCommandList->RSSetScissorRects(1, &mScissorRect);
-
-    // Transition our back buffer to be able to be used as a render target since we're rendering to it
-    mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mRenderTargets[mFrameIndex], D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
-
-    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(mRtvHeap->GetCPUDescriptorHandleForHeapStart(), mFrameIndex, mRtvDescriptorSize);
-    mCommandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
+    commandList->SetGraphicsRootSignature(mRootSignature);
+    commandList->SetPipelineState(mPipelineState);
+    commandList->RSSetViewports(1, &mViewport);
+    commandList->RSSetScissorRects(1, &mScissorRect);
 
     // This is the actual stuff we are drawing
-    const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
-    mCommandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
-    mCommandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    mCommandList->IASetVertexBuffers(0, 1, &mVertexBufferView);
-    mCommandList->DrawInstanced(3, 1, 0, 0);
-
-    // Transition back buffer back to the present state since we are done drawing to it and want it ready for present
-    mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mRenderTargets[mFrameIndex], D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
-
-    ensure(SUCCEEDED(mCommandList->Close()));
-
-    ID3D12CommandList* ppCommandLists[] = { mCommandList };
-    mCommandQueue->ExecuteCommandLists(1, ppCommandLists);
-}
-
-void RendererImpl::Present()
-{
-    ensure(SUCCEEDED(mSwapChain->Present(1, 0)));
-}
-
-void RendererImpl::WaitForPreviousFrame()
-{
-    // TODO: This is apparently not a good way to queue up work for the GPU because we're doing nothing while we wait for the previous frame to render.
-    // Look into the frame buffering sample
-
-    // Signal and increment the fence value.
-    const UINT64 fence = mFenceValue;
-    ensure(SUCCEEDED(mCommandQueue->Signal(mFence, fence)));
-    mFenceValue++;
-
-    // Wait until the previous frame is finished.
-    if (mFence->GetCompletedValue() < fence)
-    {
-        ensure(SUCCEEDED(mFence->SetEventOnCompletion(fence, mFenceEvent)));
-        WaitForSingleObject(mFenceEvent, INFINITE);
-    }
-
-    mFrameIndex = mSwapChain->GetCurrentBackBufferIndex();
+    commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    commandList->IASetVertexBuffers(0, 1, &mVertexBufferView);
+    commandList->DrawInstanced(3, 1, 0, 0);
 }
