26c6cb3afe37b7d2e49ddf213e1655c2f327d5b1
Kiran Purushothaman <projects@kirankp.com>
2024-12-11 22:31:37 -0800

    Add helper to load a text file into a string

---
+++ src/Util.cpp
@@ -0,0 +1,22 @@
+#include <Util.h>
+
+#include <fstream>
+
+auto slurp(std::string_view path) -> std::string {
+    constexpr auto read_size = std::size_t(4096);
+    auto stream = std::ifstream(path.data());
+    stream.exceptions(std::ios_base::badbit);
+
+    if (!stream) {
+        throw std::ios_base::failure("File does not exist");
+    }
+
+    auto out = std::string();
+    auto buf = std::string(read_size, '\0');
+    while (stream.read(& buf[0], read_size)) {
+        out.append(buf, 0, stream.gcount());
+    }
+
+    out.append(buf, 0, stream.gcount());
+    return out;
+}
