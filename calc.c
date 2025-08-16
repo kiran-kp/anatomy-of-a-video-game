671f665dc20b634ceb8926eedd49a6a71e513148
Kiran Purushothaman <projects@kirankp.com>
2025-08-16 13:55:22 -0700

    Reorder functions

--- calc.c
+++ calc.c
@@ -15,6 +15,207 @@
 #include <SDL3_image/SDL_image.h>
 
 // ------------------------------------------------------------------------------------------
+// Layout
+// ------------------------------------------------------------------------------------------
+
+static const int FONT_ID_BODY_16 = 0;
+static const Clay_Color COLOR_WHITE = { 255, 255, 255, 255};
+static const Clay_Color COLOR_BACKGROUND = (Clay_Color) {23, 23, 23, 255};
+static const Clay_Color COLOR_NUMBER_BUTTON = (Clay_Color) {38, 38, 38, 255};
+static const Clay_Color COLOR_OPERATION_BUTTON = (Clay_Color) {24, 24, 27, 255};
+static const Clay_Color COLOR_SPECIAL_OPERATION_BUTTON = (Clay_Color) {30, 64, 175, 255};
+
+void RenderNumberButton(Clay_String text) {
+    CLAY({
+        .layout = { .sizing = { .height = CLAY_SIZING_GROW(0), .width = CLAY_SIZING_GROW(0) }, .padding = { 16, 16, 8, 8 }},
+        .backgroundColor = COLOR_NUMBER_BUTTON,
+        .cornerRadius = CLAY_CORNER_RADIUS(5)
+    }) {
+        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = 16,
+            .textColor = { 255, 255, 255, 255 }
+        }));
+    }
+}
+
+void RenderOperationButton(Clay_String text) {
+    CLAY({
+        .layout = { .sizing = { .height = CLAY_SIZING_GROW(0), .width = CLAY_SIZING_GROW(0) }, .padding = { 16, 16, 8, 8 }},
+        .backgroundColor = COLOR_OPERATION_BUTTON,
+        .cornerRadius = CLAY_CORNER_RADIUS(5)
+    }) {
+        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = 16,
+            .textColor = { 255, 255, 255, 255 }
+        }));
+    }
+}
+
+void RenderSpecialOperationButton(Clay_String text) {
+    CLAY({
+        .layout = { .sizing = { .height = CLAY_SIZING_GROW(0), .width = CLAY_SIZING_GROW(0) }, .padding = { 16, 16, 8, 8 }},
+        .backgroundColor = COLOR_SPECIAL_OPERATION_BUTTON,
+        .cornerRadius = CLAY_CORNER_RADIUS(5)
+    }) {
+        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = 16,
+            .textColor = { 255, 255, 255, 255 }
+        }));
+    }
+}
+
+typedef struct {
+    intptr_t offset;
+    intptr_t memory;
+} CalcArena;
+
+typedef struct {
+    CalcArena frameArena;
+} CalcData;
+
+CalcData CalcData_Initialize() {
+    CalcData data = {
+        .frameArena = { .memory = (intptr_t)malloc(1024) }
+    };
+
+    return data;
+}
+
+Clay_RenderCommandArray CalcCreateLayout(CalcData *data) {
+    data->frameArena.offset = 0;
+
+    Clay_BeginLayout();
+
+    Clay_Sizing layoutExpand = {
+        .width = CLAY_SIZING_GROW(0),
+        .height = CLAY_SIZING_GROW(0)
+    };
+
+    Clay_Color contentBackgroundColor = { 90, 90, 90, 255 };
+
+    // Build UI here
+    CLAY({
+        .id = CLAY_ID("OuterContainer"),
+        .backgroundColor = {43, 41, 51, 255 },
+        .layout = {
+            .layoutDirection = CLAY_TOP_TO_BOTTOM,
+            .sizing = layoutExpand,
+            .padding = CLAY_PADDING_ALL(8),
+            .childGap = 8
+        }
+    }) {
+        // Child elements go inside braces
+        CLAY({
+            .id = CLAY_ID("Output"),
+            .layout = {
+                .sizing = {
+                    .height = CLAY_SIZING_FIXED(300),
+                    .width = CLAY_SIZING_GROW(0)
+                },
+                .padding = { 16, 16, 0, 0 },
+                .childGap = 16,
+                .childAlignment = {
+                    .y = CLAY_ALIGN_Y_CENTER
+                }
+            },
+            .backgroundColor = contentBackgroundColor,
+            .cornerRadius = CLAY_CORNER_RADIUS(8)
+        }) {
+        }
+
+        CLAY({
+            .id = CLAY_ID("InputControls"),
+            .layout = { .sizing = layoutExpand, .childGap = 8 }
+        }) {
+            CLAY({
+                .id = CLAY_ID("MainContent"),
+                .backgroundColor = contentBackgroundColor,
+                .clip = { .vertical = true, .childOffset = Clay_GetScrollOffset() },
+                .layout = {
+                    .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                    .childGap = 8,
+                    .padding = CLAY_PADDING_ALL(8),
+                    .sizing = layoutExpand
+                }
+            }) {
+                CLAY({
+                    .id = CLAY_ID("Buttons"),
+                    .layout = {
+                        .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                        .childGap = 8,
+                        .sizing = layoutExpand
+                }}) {
+                    CLAY({
+                        .id = CLAY_ID("ButtonsRow0"),
+                        .layout = {
+                             .layoutDirection = CLAY_LEFT_TO_RIGHT,
+                             .childGap = 8,
+                             .sizing = layoutExpand
+                    }}) {
+                        RenderNumberButton(CLAY_STRING("7"));
+                        RenderNumberButton(CLAY_STRING("8"));
+                        RenderNumberButton(CLAY_STRING("9"));
+                        RenderOperationButton(CLAY_STRING("÷"));
+                        RenderOperationButton(CLAY_STRING(""));
+                    }
+
+                    CLAY({
+                        .id = CLAY_ID("ButtonsRow1"),
+                        .layout = {
+                            .layoutDirection = CLAY_LEFT_TO_RIGHT,
+                            .childGap = 8,
+                            .sizing = layoutExpand
+                    }}) {
+                        RenderNumberButton(CLAY_STRING("4"));
+                        RenderNumberButton(CLAY_STRING("5"));
+                        RenderNumberButton(CLAY_STRING("6"));
+                        RenderOperationButton(CLAY_STRING("×"));
+                        RenderOperationButton(CLAY_STRING(""));
+                    }
+
+                    CLAY({
+                        .id = CLAY_ID("ButtonsRow2"),
+                        .layout = {
+                            .layoutDirection = CLAY_LEFT_TO_RIGHT,
+                            .childGap = 8,
+                            .sizing = layoutExpand
+                    }}) {
+                        RenderNumberButton(CLAY_STRING("1"));
+                        RenderNumberButton(CLAY_STRING("2"));
+                        RenderNumberButton(CLAY_STRING("3"));
+                        RenderOperationButton(CLAY_STRING("-"));
+                        RenderOperationButton(CLAY_STRING(""));
+                    }
+
+                    CLAY({
+                        .id = CLAY_ID("ButtonsRow3"),
+                        .layout = {
+                            .layoutDirection = CLAY_LEFT_TO_RIGHT,
+                            .childGap = 8,
+                            .sizing = layoutExpand
+                    }}) {
+                        RenderNumberButton(CLAY_STRING("0"));
+                        RenderOperationButton(CLAY_STRING("."));
+                        RenderOperationButton(CLAY_STRING("%"));
+                        RenderOperationButton(CLAY_STRING("+"));
+                        RenderSpecialOperationButton(CLAY_STRING("="));
+                    }
+                }
+            }
+        }
+    }
+
+    Clay_RenderCommandArray renderCommands = Clay_EndLayout();
+    for (int32_t i = 0; i < renderCommands.length; i++) {
+        // Clay_RenderCommandArray_Get(&renderCommands, i)->boundingBox.y += data->yOffset;
+    }
+    return renderCommands;
+}
+
+// ------------------------------------------------------------------------------------------
 // Renderer
 // ------------------------------------------------------------------------------------------
 
@@ -274,207 +475,6 @@ static void SDL_Clay_RenderClayCommands(Clay_SDL3RendererData *rendererData, Cla
                 SDL_Log("Unknown render command type: %d", rcmd->commandType);
         }
     }
-}
-
-// ------------------------------------------------------------------------------------------
-// Layout
-// ------------------------------------------------------------------------------------------
-
-static const int FONT_ID_BODY_16 = 0;
-static const Clay_Color COLOR_WHITE = { 255, 255, 255, 255};
-static const Clay_Color COLOR_BACKGROUND = (Clay_Color) {23, 23, 23, 255};
-static const Clay_Color COLOR_NUMBER_BUTTON = (Clay_Color) {38, 38, 38, 255};
-static const Clay_Color COLOR_OPERATION_BUTTON = (Clay_Color) {24, 24, 27, 255};
-static const Clay_Color COLOR_SPECIAL_OPERATION_BUTTON = (Clay_Color) {30, 64, 175, 255};
-
-void RenderNumberButton(Clay_String text) {
-    CLAY({
-        .layout = { .sizing = { .height = CLAY_SIZING_GROW(0), .width = CLAY_SIZING_GROW(0) }, .padding = { 16, 16, 8, 8 }},
-        .backgroundColor = COLOR_NUMBER_BUTTON,
-        .cornerRadius = CLAY_CORNER_RADIUS(5)
-    }) {
-        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
-            .fontId = FONT_ID_BODY_16,
-            .fontSize = 16,
-            .textColor = { 255, 255, 255, 255 }
-        }));
-    }
-}
-
-void RenderOperationButton(Clay_String text) {
-    CLAY({
-        .layout = { .sizing = { .height = CLAY_SIZING_GROW(0), .width = CLAY_SIZING_GROW(0) }, .padding = { 16, 16, 8, 8 }},
-        .backgroundColor = COLOR_OPERATION_BUTTON,
-        .cornerRadius = CLAY_CORNER_RADIUS(5)
-    }) {
-        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
-            .fontId = FONT_ID_BODY_16,
-            .fontSize = 16,
-            .textColor = { 255, 255, 255, 255 }
-        }));
-    }
-}
-
-void RenderSpecialOperationButton(Clay_String text) {
-    CLAY({
-        .layout = { .sizing = { .height = CLAY_SIZING_GROW(0), .width = CLAY_SIZING_GROW(0) }, .padding = { 16, 16, 8, 8 }},
-        .backgroundColor = COLOR_SPECIAL_OPERATION_BUTTON,
-        .cornerRadius = CLAY_CORNER_RADIUS(5)
-    }) {
-        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
-            .fontId = FONT_ID_BODY_16,
-            .fontSize = 16,
-            .textColor = { 255, 255, 255, 255 }
-        }));
-    }
-}
-
-typedef struct {
-    intptr_t offset;
-    intptr_t memory;
-} CalcArena;
-
-typedef struct {
-    CalcArena frameArena;
-} CalcData;
-
-CalcData CalcData_Initialize() {
-    CalcData data = {
-        .frameArena = { .memory = (intptr_t)malloc(1024) }
-    };
-
-    return data;
-}
-
-Clay_RenderCommandArray CalcCreateLayout(CalcData *data) {
-    data->frameArena.offset = 0;
-
-    Clay_BeginLayout();
-
-    Clay_Sizing layoutExpand = {
-        .width = CLAY_SIZING_GROW(0),
-        .height = CLAY_SIZING_GROW(0)
-    };
-
-    Clay_Color contentBackgroundColor = { 90, 90, 90, 255 };
-
-    // Build UI here
-    CLAY({
-        .id = CLAY_ID("OuterContainer"),
-        .backgroundColor = {43, 41, 51, 255 },
-        .layout = {
-            .layoutDirection = CLAY_TOP_TO_BOTTOM,
-            .sizing = layoutExpand,
-            .padding = CLAY_PADDING_ALL(8),
-            .childGap = 8
-        }
-    }) {
-        // Child elements go inside braces
-        CLAY({
-            .id = CLAY_ID("Output"),
-            .layout = {
-                .sizing = {
-                    .height = CLAY_SIZING_FIXED(300),
-                    .width = CLAY_SIZING_GROW(0)
-                },
-                .padding = { 16, 16, 0, 0 },
-                .childGap = 16,
-                .childAlignment = {
-                    .y = CLAY_ALIGN_Y_CENTER
-                }
-            },
-            .backgroundColor = contentBackgroundColor,
-            .cornerRadius = CLAY_CORNER_RADIUS(8)
-        }) {
-        }
-
-        CLAY({
-            .id = CLAY_ID("InputControls"),
-            .layout = { .sizing = layoutExpand, .childGap = 8 }
-        }) {
-            CLAY({
-                .id = CLAY_ID("MainContent"),
-                .backgroundColor = contentBackgroundColor,
-                .clip = { .vertical = true, .childOffset = Clay_GetScrollOffset() },
-                .layout = {
-                    .layoutDirection = CLAY_TOP_TO_BOTTOM,
-                    .childGap = 8,
-                    .padding = CLAY_PADDING_ALL(8),
-                    .sizing = layoutExpand
-                }
-            }) {
-                CLAY({
-                    .id = CLAY_ID("Buttons"),
-                    .layout = {
-                        .layoutDirection = CLAY_TOP_TO_BOTTOM,
-                        .childGap = 8,
-                        .sizing = layoutExpand
-                }}) {
-                    CLAY({
-                        .id = CLAY_ID("ButtonsRow0"),
-                        .layout = {
-                             .layoutDirection = CLAY_LEFT_TO_RIGHT,
-                             .childGap = 8,
-                             .sizing = layoutExpand
-                    }}) {
-                        RenderNumberButton(CLAY_STRING("7"));
-                        RenderNumberButton(CLAY_STRING("8"));
-                        RenderNumberButton(CLAY_STRING("9"));
-                        RenderOperationButton(CLAY_STRING("÷"));
-                        RenderOperationButton(CLAY_STRING(""));
-                    }
-
-                    CLAY({
-                        .id = CLAY_ID("ButtonsRow1"),
-                        .layout = {
-                            .layoutDirection = CLAY_LEFT_TO_RIGHT,
-                            .childGap = 8,
-                            .sizing = layoutExpand
-                    }}) {
-                        RenderNumberButton(CLAY_STRING("4"));
-                        RenderNumberButton(CLAY_STRING("5"));
-                        RenderNumberButton(CLAY_STRING("6"));
-                        RenderOperationButton(CLAY_STRING("×"));
-                        RenderOperationButton(CLAY_STRING(""));
-                    }
-
-                    CLAY({
-                        .id = CLAY_ID("ButtonsRow2"),
-                        .layout = {
-                            .layoutDirection = CLAY_LEFT_TO_RIGHT,
-                            .childGap = 8,
-                            .sizing = layoutExpand
-                    }}) {
-                        RenderNumberButton(CLAY_STRING("1"));
-                        RenderNumberButton(CLAY_STRING("2"));
-                        RenderNumberButton(CLAY_STRING("3"));
-                        RenderOperationButton(CLAY_STRING("-"));
-                        RenderOperationButton(CLAY_STRING(""));
-                    }
-
-                    CLAY({
-                        .id = CLAY_ID("ButtonsRow3"),
-                        .layout = {
-                            .layoutDirection = CLAY_LEFT_TO_RIGHT,
-                            .childGap = 8,
-                            .sizing = layoutExpand
-                    }}) {
-                        RenderNumberButton(CLAY_STRING("0"));
-                        RenderOperationButton(CLAY_STRING("."));
-                        RenderOperationButton(CLAY_STRING("%"));
-                        RenderOperationButton(CLAY_STRING("+"));
-                        RenderSpecialOperationButton(CLAY_STRING("="));
-                    }
-                }
-            }
-        }
-    }
-
-    Clay_RenderCommandArray renderCommands = Clay_EndLayout();
-    for (int32_t i = 0; i < renderCommands.length; i++) {
-        // Clay_RenderCommandArray_Get(&renderCommands, i)->boundingBox.y += data->yOffset;
-    }
-    return renderCommands;
 }
 
 // ------------------------------------------------------------------------------------------
