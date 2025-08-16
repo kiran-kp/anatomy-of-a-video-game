18e3eacee313f31f8ea62fd4de1f8455c7f8b37d
Kiran Purushothaman <projects@kirankp.com>
2025-08-16 13:56:20 -0700

    remove unused headers

--- calc.c
+++ calc.c
@@ -7,12 +7,6 @@
 #include "clay.h"
 
 #include <stdio.h>
-#include <stdlib.h>
-
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL.h>
-#include <SDL3_ttf/SDL_ttf.h>
-#include <SDL3_image/SDL_image.h>
 
 // ------------------------------------------------------------------------------------------
 // Layout
